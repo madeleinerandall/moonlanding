a2184daf7dbdf9e422695aa41842a2d7a286bd5d
Madeleine Daphne Randall <madeleinedaphnerandall@Madeleines-MacBook-Air.local>
2022-07-09 13:38:38 +0100

    Bug fix on mobile version Hero section button was not yet linked

--- src/Hero/index.js
+++ src/Hero/index.js
@@ -1,16 +1,22 @@
 import "./style.scss";
+import { Link } from "react-router-dom";
 
 function Hero() {
+  function scroll() {}
   return (
     <section id="hero">
-      <div className="box">
+      <div onClick={scroll} className="box">
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <div className="call-to-action-button">
-        <button id="call-to-action-button">SHOP PRODUCTS</button>
-      </div>
+      <Link
+        to="products"
+        id="call-to-action-button"
+        className="call-to-action-button"
+      >
+        SHOP PRODUCTS
+      </Link>
     </section>
   );
 }
